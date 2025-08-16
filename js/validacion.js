e9e121e5f0bb8daed7a6d99a36ba5ecb84fb45e5
Kevin Lopetegui <you@example.com>
2025-08-16 00:48:56 -0300

    camboi

--- js/validacion.js
+++ js/validacion.js
@@ -5,3 +5,24 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
+
+let button = document.getElementById("regBtn");
+
+button.addEventListener("click", function () {
+    let nombre = document.getElementById("nombre").value;
+    let apellido = document.getElementById("apellido").value;
+    let email = document.getElementById("email").value;
+    let password1 = document.getElementById("password1").value;
+    let password2 = document.getElementById("password2").value;
+    let terminos = document.getElementById("terminos").checked;
+    
+    if (nombre == "" || apellido == ""  || email == "" || password1 == "" || password2 == "" || !terminos || password1 !== password2 || password1.length < 6) {
+        showAlertError();
+    } else {
+        showAlertSuccess();
+    }
+});
+
+
+
+
